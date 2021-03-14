bb5b6935dc8c9ad79a32359954daac685ce4afce
mhodali <79901571+mhodali@users.noreply.github.com>
2021-03-14 12:51:11 +0200

    Update create-server.sh

--- create-server.sh
+++ create-server.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #docker build /home/task3/server/ -t server
-if [ "$(docker ps -a | grep server)" ]; then
-  docker stop $(docker ps -a | grep server |awk '{print  $1}')
-  docker rm $(docker ps -a | grep server |awk '{print  $1}')
+if [ "$(docker ps -a | grep mhodali/server)" ]; then
+  docker stop $(docker ps -a | grep mhodali/server |awk '{print  $1}')
+  docker rm $(docker ps -a | grep mhodali/server |awk '{print  $1}')
 fi
-docker run -dit -p 8899:8899 -t server
+docker run -dit -p 8899:8899 -t mhodali/server
